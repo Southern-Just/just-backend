1761c4479572457e93b63b7fedf85f862dca50cb
Southern-Just <karuguezekiel@gmail.com>
2025-02-20 08:00:35 +0300

    middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,17 @@
-import {Router} from 'express';
+import { Router } from "express";
+
+import authorize from "../middlewares/auth.middleware.js";
+import { getUser, getUsers } from "../controllers/user.controllers.js";
 
 const userRoutes = Router();
 
-userRoutes.get('/', (req, res)=> res.send({title: 'GET all users'}));
+userRoutes.get("/", getUsers);
+userRoutes.get("/:id", authorize, getUser);
 
-userRoutes.get('/:id', (req, res)=> res.send({title: 'GET user details'}));
+userRoutes.post("/", (req, res) => res.send({ title: "CREATE new user" }));
 
-userRoutes.post('/', (req, res)=> res.send({title: 'CREATE new user'}));
+userRoutes.put("/:id", (req, res) => res.send({ title: "UPDATE user" }));
 
-userRoutes.put('/:id', (req, res)=> res.send({title: 'UPDATE user'}));
-
-userRoutes.delete('/:id', (req, res)=> res.send({title: 'DELETE user'}));
+userRoutes.delete("/:id", (req, res) => res.send({ title: "DELETE user" }));
 
 export default userRoutes;
