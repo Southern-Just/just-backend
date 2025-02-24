77cf75d832aa4ecdee0f19223a8871512fec8417
Southern-Just <karuguezekiel@gmail.com>
2025-02-24 04:36:45 +0300

    debugging

--- config/arcjet.js
+++ config/arcjet.js
@@ -1,8 +1,10 @@
 import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
-import { ARCJET_KEY } from ".env.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const aj = arcjet({
-  key: ARCJET_KEY,
+  key: process.env.ARCJET_KEY,
   characteristics: ["ip.src"],
   rules: [
     shield({ mode: "LIVE" }),
@@ -12,10 +14,11 @@ const aj = arcjet({
     }),
     tokenBucket({
       mode: "LIVE",
-      refillRate: 5,
-      interval: 10,
-      capacity: 10,
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
     }),
   ],
 });
+
 export default aj;
--- database/mongoDb.js
+++ database/mongoDb.js
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import {DB_URL, NODE_ENV} from "../config/.env.js";
+import { DB_URL, NODE_ENV } from "../config/.env.js";
 
-if(!DB_URL){
-    throw new Error('please define MONGODB_URL environment variable inside .env.<development/production>.local');
+if (!DB_URL) {
+  throw new Error(
+    "please define MONGODB_URL environment variable inside .env.<development/production>.local",
+  );
 }
 const connectToDatabase = async () => {
-    try {
-
-        await mongoose.connect(DB_URL);
-        console.log(`Connected to database in ${NODE_ENV} mode`);
-    }catch(error){
-        console.log('Error connecting to database: ', error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(DB_URL);
+    console.log(`Connected to database in ${NODE_ENV} mode`);
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+    process.exit(1);
+  }
+};
 export default connectToDatabase;
