d15e983011d25929e2967fa24e21f464c295572a
Southern-Just <karuguezekiel@gmail.com>
2025-03-06 05:49:25 +0300

    workflows

--- controllers/workflow.controllers.js
+++ controllers/workflow.controllers.js
@@ -1,1 +1,18 @@
-export const sendReminders = serve();
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { serve } = require("@upstash/workflow/express");
+
+export const sendReminders = serve(async (context) => {
+  const { membershipId } = context.requestPayload;
+  const membership = await fetchMembership(context, membershipId);
+
+  if (!membership || membership.status != active) return;
+
+  const renewalDate = new Date();
+});
+
+const fetchMembership = async (context, membershipId) => {
+  return await context.run("get membership", () => {
+    return Membership.findById(membershipId).populate("user", "name email");
+  });
+};
